c6765924a4c8ffa2076aab9656744320cb63806f
capybarin <vladibzd@gmail.com>
2019-11-19 18:37:11 +0200

    Random added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,27 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
 
+    public static String getRandom(String[] array) {
+        int rnd = new Random().nextInt(array.length);
+        return array[rnd];
+    }
+
     public static void main(String[] args) {
+        final int ITEMS = 6;
+        ArrayList<Item> items = new ArrayList<>();
 	    Husband hus = new Husband();
 	    hus.getShopItems(new Wife());
-	    Item item1 = new Item("Samsung",25);
-        Item item2 = new Item("Sony",30);
-        Item item3 = new Item("Potato",40);
-        Item item4 = new Item("Bread",60);
-        Item item5 = new Item("Apple",17);
-        Item item6 = new Item("Milk",10);
-        Item item7 = new Item("Acer",7);
-        ArrayList<Item> items = new ArrayList<>();
-        items.add(item1);
-        items.add(item2);
-        items.add(item3);
-        items.add(item4);
-        items.add(item5);
-        items.add(item6);
-        items.add(item7);
+	    String[] namesArray = {"Samsung","Sony","Sharp","Apple","Acer",
+                            "Bread","Sausage","Butter", "Milk","Potato"};
+        for (int i = 0; i < ITEMS; i++) {
+            int price = new Random().nextInt(300);
+            Item item = new Item(getRandom(namesArray),price);
+            items.add(item);
+        }
 	    hus.allItems(items);
 	    hus.showAll();
 	    hus.showPurchasableItems();
